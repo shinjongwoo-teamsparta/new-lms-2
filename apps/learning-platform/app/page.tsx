45486078c3abefad01ac34fdd2e47a09b3c37010
shinjw <jongwoo.shin@teamsparta.co>
2025-09-01 10:31:16 +0900

    change to multiply

--- apps/learning-platform/app/page.tsx
+++ apps/learning-platform/app/page.tsx
@@ -1,4 +1,4 @@
-import { pow } from '@new-lms2/math-utils'
+import { multiply, pow } from '@new-lms2/math-utils'
 import Image from 'next/image'
 
 export default function Home() {
@@ -29,7 +29,7 @@ export default function Home() {
         </div>
       </div>
 
-      <p>{pow(90, 2)}</p>
+      <p>{multiply(10, 2)}</p>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
